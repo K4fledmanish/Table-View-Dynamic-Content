5d74d2a4f423debc47b34897811bc3f399e19212
K4fledmanish <K4fledmanish@aol.com>
2020-09-13 15:07:06 +1000

    Initial Commit

---
+++ Table-View-Dynamic-Content/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Table-View-Dynamic-Content
+//
+//  Created by K4fle D. Manish on 13/9/20.
+//  Copyright © 2020 K4fledmanish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
